ff4e0b5ae75666dce27ccf7e89f01a3c2c591071
Saranya <Saranya.Vinayagam@cognizant.com>
2025-05-28 15:35:31 +0100

    test connection using sftp:// schema

--- src/routes/connectSftpViaProxy.js
+++ src/routes/connectSftpViaProxy.js
@@ -14,9 +14,9 @@ const logger = createLogger()
  */
 
 export async function connectSftpThroughProxy() {
-  // const proxyUrl = new URL(config.get('httpProxy'))
-  const proxyHost = 'localhost'
-  const proxyPort = 3128
+  const proxyUrl = new URL(config.get('httpProxy'))
+  const proxyHost = proxyUrl.hostname
+  const proxyPort = proxyUrl.port || 3128
   logger.info(`port::: ${proxyPort}`)
   const sftpHost = 'sftp22.sftp-defra-gov-uk.quatrix.it'
   const sftpPort = 22
@@ -28,9 +28,9 @@ export async function connectSftpThroughProxy() {
     host: proxyHost,
     port: proxyPort,
     method: 'CONNECT',
-    path: `sftp22.sftp-defra-gov-uk.quatrix.it:22`,
+    path: `sftp://${sftpHost}:${sftpPort}`,
     headers: {
-      Host: `sftp22.sftp-defra-gov-uk.quatrix.it:22`
+      Host: `${sftpHost}:${sftpPort}`
       // 'Proxy-Authorization': proxyAuthHeader
     }
     // rejectUnauthorized: false // Disable certificate validation
@@ -47,8 +47,12 @@ export async function connectSftpThroughProxy() {
   return new Promise((resolve, reject) => {
     logger.info(`inside Promise`)
     logger.info(`privateKey:: ${privateKey}`)
-    const req = proxyModule.request(proxyOptions)
-    logger.info(`REQUEST:: ${JSON.stringify(req)}`)
+    const req = http.request(proxyOptions)
+    logger.info(`BEFORE REQUEST:: ${JSON.stringify(req)}`)
+    logger.info(`BEFORE PATH:: ${req.path}`)
+    req.path = `sftp://${sftpHost}:${sftpPort}`
+    logger.info(`AFTER PATH:: ${req.path}`)
+    logger.info(`AFTER REQUEST:: ${JSON.stringify(req)}`)
     req.on('connect', (res, socket) => {
       logger.info(`SOCKET:: ${JSON.stringify(socket)}`)
       logger.info(`RESPONSE:: ${JSON.stringify(res)}`)
