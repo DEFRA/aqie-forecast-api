bcc7a6696bf95dfe53ef32b5e55dc610d87f7cc8
Saranya Vinayagam <saranya.vinayagam@cognizant.com>
2025-09-03 15:07:03 +0100

    Update utility.js

--- src/forecast/helpers/utility.js
+++ src/forecast/helpers/utility.js
@@ -2,8 +2,8 @@
 
 export const getExpectedFileName = () => {
   // const today = dayjs().format('YYYYMMDD')
-  // return `MetOfficeDefraAQSites_${today}.xml` // //MetOfficeDefraAQSites_20250425.xml
-  return `MetOfficeDefraAQSites_20250902.xml`
+  return `MetOfficeDefraAQSites_${today}.xml` // //MetOfficeDefraAQSites_20250425.xml
+  // return `MetOfficeDefraAQSites_20250902.xml`
 }
 
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
